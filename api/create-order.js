3e635e392227921192ad319c44e558a24e702c12
adwaidhanilofficial-cell <Adwaidhanil.official@gmail.com>
2026-03-24 00:21:25 +0530

    fix: convert api/create-order.js to ES module to prevent 500 crash

--- api/create-order.js
+++ api/create-order.js
@@ -1,6 +1,6 @@
-const Razorpay = require('razorpay');
+import Razorpay from 'razorpay';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
